ad5fefdbca47eaebb900ffd86573f9e275a40160
irazramin <irazramin@gmail.com>
2022-05-23 11:11:46 +0600

    single order api added

--- index.js
+++ index.js
@@ -84,7 +84,12 @@ async function run() {
     })
 
 
-
+    app.get('/order/:id', verifyJwt, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollections.findOne(query);
+      res.send(result);
+    });
 
 
 
