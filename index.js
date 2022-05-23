e85d67fbb34dadaa85abfcb7c49636ccfc1a5006
irazramin <irazramin@gmail.com>
2022-05-23 11:11:16 +0600

    jwt verify added

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 
+
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9vbqe.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -27,6 +31,21 @@ async function run() {
   const orderCollections = client.db('toolsDb').collection('orderCollection');
 
 
+
+  const verifyJwt = (req, res, next) => {
+    const header = req.headers.authorization;
+    if (!header) {
+      return res.status(401).send({ message: 'unauthorized' });
+    }
+    const token = header.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'forbidden' });
+      }
+      req.decoded = decoded;
+      next();
+    });
+  };
   try {
 
     app.get('/tools', async (req, res) => {
@@ -58,10 +77,22 @@ async function run() {
         res.send({result,accessToken:token});
     });
 
-    app.post('/order',async(req,res) =>{
+    app.post('/order', async(req,res) =>{
       const order = req.body;
       const result = await orderCollections.insertOne(order);
       res.send({result,success:true});
+    })
+
+
+
+
+
+
+    app.get('/order/:email',verifyJwt,async(req,res) =>{
+      const email = req.params.email;
+      const query = {email};
+      const result = await orderCollections.find(query).toArray();
+      res.send(result);
     })
   }
   finally{
