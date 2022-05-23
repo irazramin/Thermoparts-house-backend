1ba8f35cf66125dd7df544f1a25e9c216143fdab
irazramin <irazramin@gmail.com>
2022-05-23 16:05:56 +0600

    payment api added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -40,6 +40,22 @@ async function run() {
   const orderCollections = client.db('toolsDb').collection('orderCollection');
 
   try {
+
+    app.post('/create-payment-intent',async(req,res) =>{
+      const product = req.body;
+      const price= product.totalAmount;
+      const amount = price*100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount:amount,
+        currency:"usd",
+        payment_method_types:["card"]
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    })
+
+
+
     app.get('/tools', async (req, res) => {
       const query = {};
       const result = await toolCollections.find(query).toArray();
@@ -87,7 +103,9 @@ async function run() {
       const query = {_id:ObjectId(id)}
       const result = await orderCollections.findOne(query);
       res.send(result);
-    })
+    });
+
+ 
   } finally {
   }
 }
